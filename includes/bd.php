b485fe29debee6fc0ec81c774901d45eea72abd0
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-02-02 23:45:47 -0300

    atualização form cadastro cliente

---
+++ includes/bd.php
@@ -0,0 +1,57 @@
+<?php
+
+$arrCategoria = array(
+    "Foods / Delivery",
+    "E-commerce / Loja Virtual",
+    "Ensino / Educação",
+    "Construção / Reformas",
+    "Indústria / Fábrica",
+    "Comércio",
+    "Serviços",
+);
+
+$arrSubCategoria = array(
+    "Info-Produtos",
+    "Lavanderia",
+    "Marcenaria",
+    "Bares",
+    "Restaurante",
+    "Panificadora",
+    "Delivery",
+    "Eventos",
+    "E-commerce",
+    "Dropshiping",
+    "Loja Virtual",
+    "Mercados",
+    "Mecânica",
+    "Assistência Técnica",
+    "Construção",
+    "Ensino",
+    "Academia",
+    "Advocacia",
+    "Concessionaria",
+    "Consultório",
+    "Pizzaria",
+    "Hamburgeria",
+    "Eventos",
+    "Eventos",
+    "Eventos",
+);
+
+$arrTipo = array(
+    "Lead",
+    "Prospect",
+    "Follow Up",
+    "Customer",
+);
+
+$arrFase = array(
+"Prospecção",
+"Pesquisa de Mercado",
+"Contato",
+"Apresentação",
+"Negociação",
+"Venda",
+"Logística",
+"Pós Venda",
+);
--- includes/clientes_form_cadastro.php
+++ includes/clientes_form_cadastro.php
@@ -1,219 +1,206 @@
+<?php include 'bd.php'; ?>
+
 <form method="POST" action="" autocomplete="off" enctype="multipart/form-data" class="pb-5">
 
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <label class="input-group-text" for="inputGroupSelect01">Categoria</label>
-                <select class="form-select" id="inputGroupSelect01">
-                    <option selected>Selecione uma Categoria...</option>
-                    <option value="1">Foods / Delivery</option>
-                    <option value="2">E-commerce / Loja Virtual</option>
-                    <option value="3">Ensino / Educação</option>
-                    <option value="3">Construção / Reformas</option>
-                    <option value="3">Indústria / Fábrica</option>
-                    <option value="3">Comércio</option>
-                    <option value="3">Serviços</option>
-                </select>
+    <fieldset class="mb-3">
+        <legend>Dados Comerciais</legend>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <label class="input-group-text" for="inputGroupSelect01">Categoria</label>
+                    <select class="form-select" id="inputGroupSelect01">
+                        <option selected>Selecione uma Categoria...</option>
+                        <?php
+                        foreach ($arrCategoria as $key => $value) {
+                            echo "<option value=" . ($key + 1) . ">" . $value . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <label class="input-group-text" for="inputGroupSelect01">Sub-Categoria</label>
+                    <select class="form-select" id="inputGroupSelect01">
+                        <option selected>Selecione uma Categoria...</option>
+                        <?php
+                        foreach ($arrSubCategoria as $key => $value) {
+                            echo "<option value=" . ($key + 1) . ">" . $value . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <label class="input-group-text" for="inputGroupSelect01">Tipo</label>
+                    <select class="form-select" id="inputGroupSelect01">
+                        <option selected>Selecione o Tipo...</option>
+                        <?php
+                        foreach ($arrTipo as $key => $value) {
+                            echo "<option value=" . ($key + 1) . ">" . $value . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <label class="input-group-text" for="inputGroupSelect01">Fase</label>
+                    <select class="form-select" id="inputGroupSelect01">
+                        <option selected>Selecione uma Fase...</option>
+                        <?php
+                        foreach ($arrFase as $key => $value) {
+                            echo "<option value=" . ($key + 1) . ">" . $value . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <label class="input-group-text" for="inputGroupSelect01">Sub-Categoria</label>
-                <select class="form-select" id="inputGroupSelect01">
-                    <option selected>Selecione uma Categoria...</option>
-                    <option value="1">Info-Produtos</option>
-                    <option value="2">Lavanderia</option>
-                    <option value="3">Marcenaria</option>
-                    <option value="3">Bares</option>
-                    <option value="3">Restaurante</option>
-                    <option value="3">Panificadora</option>
-                    <option value="3">Delivery</option>
-                    <option value="3">Eventos</option>
-                    <option value="3">E-commerce</option>
-                    <option value="3">Dropshiping</option>
-                    <option value="3">Loja Virtual</option>
-                    <option value="3">Mercados</option>
-                    <option value="3">Mecânica</option>
-                    <option value="3">Assistência técnica</option>
-                    <option value="3">Construção</option>
-                    <option value="3">Ensino</option>
-                    <option value="3">Academia</option>
-                    <option value="3">Advocacia</option>
-                    <option value="3">Concessionaria</option>
-                    <option value="3">Consultório</option>
-                    <option value="3">Pizzaria</option>
-                    <option value="3">Hamburgeria</option>
-                    <option value="3">Eventos</option>
-                    <option value="3">Eventos</option>
-                    <option value="3">Eventos</option>
-                </select>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Nome</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">E-mail</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Telefone</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <label class="input-group-text" for="inputGroupSelect01">Tipo</label>
-                <select class="form-select" id="inputGroupSelect01">
-                    <option selected>Selecione o Tipo...</option>
-                    <option value="1">Lead</option>
-                    <option value="2">Prospect</option>
-                    <option value="3">Follow Up</option>
-                    <option value="3">Customer</option>
-                </select>
+        <div class="row mb-3">
+            <div class="col-7">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Slogan</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <input type="file" class="form-control" id="inputGroupFile02">
+                    <label class="input-group-text" for="inputGroupFile02">Logo</label>
+                </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <label class="input-group-text" for="inputGroupSelect01">Fase</label>
-                <select class="form-select" id="inputGroupSelect01">
-                    <option selected>Selecione uma Fase...</option>
-                    <option value="1">Prospecção</option>
-                    <option value="2">Pesquisa de Mercado</option>
-                    <option value="3">Contato</option>
-                    <option value="3">Apresentação</option>
-                    <option value="3">Negociação</option>
-                    <option value="3">Venda</option>
-                    <option value="3">Logística</option>
-                    <option value="3">Pós Venda</option>
-                </select>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">Keywords</span>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
             </div>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Nome Comercial</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">Copywriting</span>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">E-mail Comercial</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Website</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">GMN-link</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
         </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Telefone Comercial</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-    </div>
+        <div class="row mb-3">
 
-    <div class="row mb-3">
-        <div class="col-7">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Slogan</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Facebook_link</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
-        </div>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <input type="file" class="form-control" id="inputGroupFile02">
-                <label class="input-group-text" for="inputGroupFile02">Logo</label>
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Facebook_tag</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Website</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Instagram_link</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">GMN-link</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Facebook_link</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Instagram_tag</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
         </div>
+    </fieldset>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Facebook_tag</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+    <fieldset class="mb-3">
+        <legend>Dados do Responsável</legend>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Nome</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Telefone</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Email</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
             </div>
         </div>
+    </fieldset>
 
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Instagram_link</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+    <fieldset class="mb-3">
+        <legend>Observações</legend>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text">Anotações</span>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
             </div>
         </div>
-
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Instagram_tag</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text">Keywords</span>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text">Copywriting</span>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Nome Responsável</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Telefone Responsável</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Email Responsável</span>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-            </div>
-        </div>
-    </div>
-
-    <div class="row mb-3">
-        <div class="col">
-            <div class="input-group input-group-sm">
-                <span class="input-group-text">Anotações</span>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
-        </div>
-    </div>
+    </fieldset>
 
     <div class="d-flex justify-content-evenly">
         <button type="button" class="btn btn-danger">Danger</button>
